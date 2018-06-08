50916536a069e6c92c3a4b04f27355e3b856d163
TJ Clark <mav@TJs-Mac-mini.local>
2018-06-08 09:33:51 -0700

    updated settings that were errored

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/app';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import thunk from 'redux-thunk';
+
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
+import Home from './components/home';
+import Results from './components/results';
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
+        <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/results' component={Results}/>
+        </Switch>
       </BrowserRouter>
     </Provider>
     , document.querySelector('.app-wrapper'));
